72e94558429d175b1799f8e48e2443f4bcd18602
ekurniawan <erick.kurniawan@gmail.com>
2023-04-11 10:51:03 +0700

    menambahkan command services

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST Command Service");
+            return Ok("Inbound test from Platforms Controller");
+        }
+    }
+
+}
