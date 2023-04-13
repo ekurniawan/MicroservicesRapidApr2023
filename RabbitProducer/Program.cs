3bdafffaad16a0b5a4750ac726a022191ecc59b4
ekurniawan <erick.kurniawan@gmail.com>
2023-04-13 13:38:33 +0700

    menambahkan rabbitmq producer

--- RabbitProducer/Program.cs
+++ RabbitProducer/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using RabbitProducer;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+DirectExchangePublisher.Publish(channel);
